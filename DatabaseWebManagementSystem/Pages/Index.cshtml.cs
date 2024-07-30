157f84a7188fb9cd57bbec98eca822b802017f03
Mert Muslu <96336190+mertmuslu@users.noreply.github.com>
2024-07-30 11:14:17 +0300

    Add files via upload

--- DatabaseWebManagementSystem/Pages/Index.cshtml.cs
+++ DatabaseWebManagementSystem/Pages/Index.cshtml.cs
@@ -1,20 +1,55 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
-namespace DatabaseWebManagementSystem.Pages
+namespace DatabaseWebInterface.Pages.Customers
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
+        
+        public List<CustomerInfo> listCustomers = new List<CustomerInfo>();
         public void OnGet()
         {
+            try
+            {
+                String connectionString = "Data Source=DESKTOP-07NFG6R;Initial Catalog=PersonalCareDatabase;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM Customer";
+                    using(SqlCommand command = new SqlCommand(sql, connection)) {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                CustomerInfo customerInfo = new CustomerInfo();
+                                customerInfo.CustomerID = "" + reader.GetInt32(0);
+                                customerInfo.CustomerName = reader.GetString(1);
+                                customerInfo.CustomerSurname = reader.GetString(2);
+                                customerInfo.CustomerAddress = reader.GetString(3);
+                                customerInfo.CustomerBudget = "" + reader.GetInt32(4);
+                                customerInfo.ContactNumber = reader.GetString(5);
 
+                                listCustomers.Add(customerInfo);
+                            }
+                        }
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
+    }
+    public class CustomerInfo
+    {
+        public String CustomerID;
+        public String CustomerName;
+        public String CustomerSurname;
+        public String CustomerAddress;
+        public String CustomerBudget;
+        public String ContactNumber;
     }
 }
